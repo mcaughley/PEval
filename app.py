60cc22576b7909f19e195e8e3725b3d5d6dcae09
mcaughley <mcaughley@cbkm.au>
2026-02-17 09:07:01 +1000

    Update app.py

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ if uploaded_file is not None:
             title_style.alignment = 1
             elements.append(Paragraph("PROJECT DESIGN REVIEW", title_style))
 
-            elements.append(Spacer(1, 12*mm))
+            elements.append(Spacer(1, 20*mm))
 
             elements.append(Paragraph("Commercial Use Pontoon (GCM-2136)", styles['Heading2']))
             elements.append(Spacer(1, 8*mm))
@@ -333,6 +333,7 @@ if uploaded_file is not None:
 
 else:
     st.info("Upload PDF to begin.")
+
 
 
 
